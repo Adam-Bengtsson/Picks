f99def46a95613ce3d028d524c41895b7a1b4cb5
Adam Bengtsson <info@adambengtsson.se>
2019-01-30 12:51:24 +0100

    Add project files.

---
+++ Picks.Infrastructure/CustomAppSettings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Picks.Infrastructure
+{
+    public class CustomAppSettings
+    {
+        public CustomAppSettings()
+        {
+            // Ändra värdena i appsettings.json
+            SendGridApiKey = "LoremIpsum";
+        }
+
+        public string SendGridApiKey { get; set; }
+    }
+}
---
+++ Picks.Web/Startup.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Picks.Infrastructure;
+
+namespace Picks.Web
+{
+    public class Startup
+    {
+        IConfiguration _configuration;
+
+        public Startup(IConfiguration conf)
+        {
+            _configuration = conf;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.Configure<CustomAppSettings>(_configuration.GetSection("CustomAppSettings"));
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseStatusCodePages();
+            }
+
+            app.UseStaticFiles();
+            app.UseSession();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Browse}/{action=Index}"
+                );
+            });
+        }
+    }
+}
